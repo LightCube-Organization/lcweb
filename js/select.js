86778ea442a82c0e3895aad59901b1fb83bc83d7
opera7133 <ainznino@pm.me>
2020-09-12 22:05:08 +0900

    new

--- js/select.js
+++
@@ -1,6 +0,0 @@
-$('select').change(function () {
-    var val = $('select option:selected').val();
-    if (val == 'select') return;
-    $('section').fadeOut();
-    $('section#' + val).fadeIn();
-});
